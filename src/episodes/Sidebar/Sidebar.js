7684a8a0be7ed4bdf2703870b20dff9e58bd3b1c
Adam Romanski <adam.romanski@hotjar.com>
2021-10-03 17:03:24 +0200

    Sidebar

---
+++ src/episodes/Sidebar/Sidebar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Sidebar = () => {
+    return (
+        <>
+            <button>x</button>
+            <ul>
+                <li>Lorem</li>
+                <li>Ipsum</li>
+                <li>Dolor</li>
+                <li>Sit</li>
+                <li>Amet</li>
+            </ul>
+            <div>
+                <div />
+                <div />
+                <div />
+                <div />
+            </div>
+        </>
+    )
+};
+
+export default Sidebar
